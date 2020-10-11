0eab62fc93a5bfc0af54b34052841b87bb605a0e
demlazar <demlazar@gmail.com>
2020-10-11 23:58:51 +0300

    -изменена валидация на странице "Добавление модуля" +добавлен прогресс-бар на загрузку файла

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Tewr.Blazor.FileReader;
+using MatBlazor;
 
 namespace CaptchaApp.Client
 {
@@ -15,6 +16,17 @@ namespace CaptchaApp.Client
             builder.RootComponents.Add<App>("app");
 
             builder.Services.AddFileReaderService(options => options.UseWasmSharedBuffer = true);
+
+            builder.Services.AddMatToaster(config =>
+            {
+                config.Position = MatToastPosition.BottomRight;
+                config.PreventDuplicates = true;
+                config.NewestOnTop = true;
+                config.ShowCloseButton = true;
+                config.MaximumOpacity = 95;
+                config.VisibleStateDuration = 30000;
+            });
+
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
             await builder.Build().RunAsync();
